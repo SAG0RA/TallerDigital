312bc32e88e91873155876a28ef144b67a7c3d86
Chago0900 <47864194+Chago0900@users.noreply.github.com>
2023-09-13 02:17:27 -0600

    Commit

--- Laboratorio#3/sources/paramCalculator.sv
+++ Laboratorio#3/sources/paramCalculator.sv
@@ -1,29 +1,37 @@
 
 
 module paramCalculator 
+
+	# (parameter N = 4)	// Cantidad de bits de entrada y salida.
+	
 	(
 	
-	input		logic		[3 : 0]    Z, Y		,	// switches: [6, 9] => Z, [0, 3] => Y 
+	input		logic		[N-1 : 0]    Z, Y		,	// switches: [6, 9] => Z, [0, 3] => Y 
 	
 	input		logic		[1 : 0]    mode		,	// switches: 4 y 5 para elegir modo de operacion 
 		
 	input		logic		[1 : 0]    btn_change,	// button KEY0
 	
 	output   logic    [6 : 0]    segA, segB,  // 7 segmentos
-										  segC
-	
-		
-	//output   logic    [1 : 0]    flag				// Resultado
-	
+										  segD,
+										  
+	output   logic    [N-1 : 0]  aritm_out  
+			
 	);
 	
-	logic    [7 : 0]    aritm_out;		// Resultado de operaciones aritmetricas
+	//logic    [N-1 : 0]    aritm_out;		// Resultado de operaciones aritmetricas
 	
-	logic    		     logic_out;		// Resultado de operaciones aritmetricas
+	logic    [N : 0]      temp_out;		// Variable temporal para verificar overflow
+		
+	logic    [7 : 0]  	 bcd;				// BCD para despliegue en decimal
 	
-	logic    [11 : 0]   bcd;				// BCD para despliegue en decimal
+	logic 		  	  		 flag_N, flag_Z, 
+								 flag_C, flag_V;
+								 
+	logic    [N-1 : 0]    a1, b1;
+	logic    [N   : 0]    p1;
 	
-	logic 	[1 : 0]	  flag;
+	integer i;
 	
 	// Modulo para convertir binario a BCD
 	bin2bcd uut (
@@ -35,60 +43,159 @@ module paramCalculator
 	
 	always_comb begin
 	
+		aritm_out = 0;
+		temp_out	= 0;
+		flag_N = 0;
+		flag_Z = 0;
+		flag_C = 0;
+		flag_V = 0;
+		
+		// Division variables
+		a1 = Z;
+      b1 = Y;
+      p1= 0;
+		i = 0;
+	
 		
 		case ({btn_change, mode})
 			
 			{2'b11, 2'b00}	:	begin		// ADD
 						
-										aritm_out = Z + Y;
+										temp_out = Z + Y;
 										
-										end
-			
-			{2'b11, 2'b01}	:	begin		// SUB **
-										
-										if (Z >= Y)
-											aritm_out = Z - Y;
-										else
+										if (temp_out > 5'h0_F)
 											begin
-											aritm_out = Y - Z;
-											//segD = 7'b0000001;
-											//flag = 2'b00; 		// Caso Negativo: N 
+												aritm_out = 0;
+												flag_C = 1;
+											end
+											
+										else if (temp_out == 0)
+											flag_Z = 1;
+											
+										else
+											aritm_out = temp_out;
+											
+									 end
+			
+			{2'b11, 2'b01}	:	begin		// SUB 
+										
+										if (Z < Y)
+											aritm_out = Z - Y;
+											
+										else if (Y == Z)
+											flag_Z = 1;
+										
+										else if (Z > Y)
+											begin
+												aritm_out = Y - Z;
+												flag_N = 1; 			// Caso Negativo: N 
 											end
 										end
 			
 			{2'b11, 2'b10}	:	begin		//MULT
 						
-										aritm_out = Z * Y;
+										temp_out = Z * Y;
+										
+										if (temp_out > 5'h0_F)
+											begin
+												aritm_out = 0;
+												flag_C = 1;
+											end
+											
+										else if (Z == 0  || Y == 0)
+											begin
+												flag_Z = 1;
+												aritm_out = 0;
+											end
+											
+										else
+											aritm_out = temp_out;
+										
 										
 										end
 			
 			{2'b11, 2'b11}	:	begin		//DIV **
 						
-										aritm_out = Z / Y;
+										
+										if (Y == 0)
+											begin
+												flag_V = 1;
+												aritm_out = 0;
+											end
+											
+										else if (Z == 0)
+											begin
+												flag_Z = 1;
+												aritm_out = 0;
+											end
+
+										else  
+											begin
+											for(i=0;i < N;i=i+1)    
+												begin //start the for loop
+													p1 = {p1[N-2:0],a1[N-1]};
+													a1[N-1:1] = a1[N-2:0];
+													p1 = p1-b1;
+													if(p1[N-1] == 1)    
+														begin
+														 a1[0] = 0;
+														 p1 = p1 + b1;   
+														end
+													else
+														a1[0] = 1;
+												end
+											end
+											aritm_out = a1;
 										
 										end	
 									
 			{2'b10, 2'b00}	:	begin		//MOD **
-						
-										aritm_out = Z % Y;
+										
+										if (Y == 0)
+											begin
+												flag_V = 1;
+												aritm_out = 0;
+											end
+											
+										else if (Z == 0)
+											begin
+												flag_Z = 1;
+												aritm_out = 0;
+											end
+
+										else
+										
+											aritm_out = Z % Y;
 										
 										end	
 									
-			{2'b10, 2'b01}	:	begin		//AND
+			{2'b10, 2'b01}	:	begin		//AND true when both its operands are true or non-zero
 						
-										logic_out = Z & Y;
+										temp_out = Z & Y;
+										
+										if (aritm_out == 0)
+											flag_Z = 1;
+											
 										
 										end	
 										
-			{2'b10, 2'b10}	:	begin		//OR 
-						
-										logic_out = Z | Y;
+			{2'b10, 2'b10}	:	begin		//OR true when either of its operands are true or non-zero
+										
+										aritm_out = Z | Y;
+										
+										if (aritm_out == 0)
+											flag_Z = 1;
+											
 										
 										end
 										
 			{2'b10, 2'b11}	:	begin		//XOR
 						
-										logic_out = Z ^ Y;
+										aritm_out = Z ^ Y;
+										
+										if (aritm_out == 0)
+											flag_Z = 1;
+											
 										
 										end
 										
@@ -96,11 +203,19 @@ module paramCalculator
 						
 										aritm_out = Z << Y;
 										
+										if (aritm_out == 0)
+											flag_Z = 1;
+											
+										
 										end
 										
 			{2'b00, 2'b01}	:	begin		//SHIFT R
 						
 										aritm_out = Z >> Y;
+										
+										if (aritm_out == 0)
+											flag_Z = 1;
+											
 										
 										end
 			default:	
@@ -138,7 +253,7 @@ module paramCalculator
 			default: segB = 7'b1111111; // OFF
 		endcase
 		
-		case (bcd[11:8])
+		/*case (bcd[11:8])
 			4'b0000: segC = 7'b0000001; //0
 			4'b0001: segC = 7'b1001111; //1
 			4'b0010: segC = 7'b0010010; //2
@@ -151,15 +266,27 @@ module paramCalculator
 			4'b1001: segC = 7'b0000100; //9
 			default: segC = 7'b1111111; // OFF
 		endcase
-		
-		/*case (flag)
-			2'b00:	segD = 7'b0000001;	// (-)
-			2'b01:	segD = 7'b0010010;   // Z
-			2'b10:	segD = 7'b1000110;   // C
-			2'b11:	segD = 7'b1000001;   // V
-			default: segD = 7'b1111111; // OFF
-		endcase
 		*/
+		
+		case ({flag_N, flag_Z, flag_C, flag_V})
+			4'b1000:	begin
+							segD = 7'b1111110;	// (-)
+						end
+			4'b0100:	begin
+							segD = 7'b0010010;   // Z
+						end
+			4'b0010:	begin
+							segD = 7'b1010101;   // C
+						end
+			4'b0001:	begin
+							segD = 7'b1000001;   // V
+						end
+			default: 
+						begin
+							segD = 7'b1111111; // OFF
+						end
+		endcase
+		
 		
 	end
 	
--- Laboratorio#3/testbenches/paramCalculator_tb.sv
+++ Laboratorio#3/testbenches/paramCalculator_tb.sv
@@ -2,17 +2,18 @@
 
 module paramCalculator_tb;
 
+	parameter N = 4;
+
 	
-	logic		[3 : 0]    Z, Y		;	// switches: [6, 9] => Z, [0, 3] => Y 
+	logic		[N-1 : 0]    Z, Y		;	// switches: [6, 9] => Z, [0, 3] => Y 
 	
 	logic		[1 : 0]    mode		;	// switches: 4 y 5 para elegir modo de operacion 
 		
-	logic				     btn_change;	// button KEY0
+	logic		[1 : 0]	  btn_change;	// button KEY0
 	
-	logic    [6 : 0]    segA, segB;
-		
-	logic    [3 : 0]    out			;
-
+	logic    [6 : 0]    segA, segB, segD;
+	
+	logic    [N-1 : 0]    aritm_out;
 	
 	paramCalculator u0 (
 		
@@ -22,17 +23,212 @@ module paramCalculator_tb;
 		.btn_change	(btn_change),
 		.segA			(segA),
 		.segB			(segB),
-		.out			(out)
+		.segD			(segD),
+		.aritm_out	(aritm_out)
 		
 		);
 		
 		initial begin
 		
-			btn_change = 1'b0;
+			// Probando ADD
+		
+			btn_change = 2'b11;
 			mode		  = 2'b00;
 			
 			Z = 4'b0001;
 			Y = 4'b0001;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando SUB
+			
+			btn_change = 2'b11;
+			mode		  = 2'b01;
+			
+			Z = 4'b0001;
+			Y = 4'b0010;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando MULT
+			
+			btn_change = 2'b11;
+			mode		  = 2'b10;
+			
+			Z = 4'b0001;
+			Y = 4'b0010;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando DIV
+			
+			btn_change = 2'b11;
+			mode		  = 2'b11;
+			
+			Z = 4'b0001;
+			Y = 4'b0010;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando MOD
+			
+			btn_change = 2'b10;
+			mode		  = 2'b00;
+			
+			Z = 4'b0001;
+			Y = 4'b0010;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando AND
+			
+			btn_change = 2'b10;
+			mode		  = 2'b01;
+			
+			Z = 4'b0001;
+			Y = 4'b0000;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando OR
+			
+			btn_change = 2'b10;
+			mode		  = 2'b10;
+			
+			Z = 4'b0000;
+			Y = 4'b0010;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando XOR
+			
+			btn_change = 2'b10;
+			mode		  = 2'b11;
+			
+			Z = 4'b0000;
+			Y = 4'b0010;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando SHIFT L
+			
+			btn_change = 2'b00;
+			mode		  = 2'b10;
+			
+			Z = 4'b0000;
+			Y = 4'b0010;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
+			
+			// Probando SHIFT R
+			
+			btn_change = 2'b00;
+			mode		  = 2'b01;
+			
+			Z = 4'b0000;
+			Y = 4'b0010;
+			
+			#50;
+			
+			Z = 4'b1000;
+			Y = 4'b1000;
+			
+			#50;
+			
+			Z = 4'b0101;
+			Y = 4'b0011;
+			
+			#50;
 			
 		
 		end
