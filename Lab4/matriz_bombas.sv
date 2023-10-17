30c58e2a54689d4138a9a712c22b61578cb1c884
03-Kevin <kejorola@gmail.com>
2023-10-17 08:54:05 -0600

    Funciones para lógica
    solo hace falta para condición de victoria

--- Lab4/matriz_bombas.sv
+++ Lab4/matriz_bombas.sv
@@ -4,7 +4,12 @@ module matriz_bombas (
   output bit m_bombas [7:0][7:0], // Declaración de la matriz m_bombas como una salida
   input integer bombas_entrada [7:0][7:0], // Matriz de bombas (cambiando el nombre)
   output integer m_perimetro [7:0][7:0], // Matriz de perimetro
-  output integer casillas_libres // Número de casillas libres
+  output integer casillas_libres, // Número de casillas libres
+  input integer posibles_bombas, // Declaración de posibles_bombas como entrada
+  input integer condicion_victoria, // Declaración de condición victoria
+  input integer movimiento, //jugada 
+  input integer posicion_x, //coordenada en x
+  input integer posicion_y //coordenada en y
 );
 
   // Otras lógicas y funciones pueden estar aquí
@@ -26,10 +31,12 @@ function void insertar_bombas(output bit m_bombas [7:0][7:0], input int num_bomb
     for (int j = 0; j < 8; j = j + 1) begin
       if (bombs_placed < num_bombas) begin
         m_bombas[i][j] = 1'b1; // Agregar una bomba
+		  
         bombs_placed = bombs_placed + 1;
         $display("Colocando bomba en (%0d, %0d)", i, j);
       end else begin
         m_bombas[i][j] = 1'b0; // Agregar un 0
+		  
       end
     end
   end
@@ -37,7 +44,6 @@ endfunction
 
 function void bombas_encontradas(input bit bombas_entrada [7:0][7:0], output integer m_perimetro [7:0][7:0]);
   integer i, j, x, y;
-
   for (i = 0; i < 8; i = i + 1) begin
     for (j = 0; j < 8; j = j + 1) begin
       m_perimetro[i][j] = 0; // Inicializar cada celda como 0 (entero)
@@ -66,6 +72,77 @@ function void cantidad_libres(input integer num_bombas, output integer casillas_
   casillas_libres = 64 - num_bombas;
 endfunction
 
+function void incrementar_bombas(inout integer posibles_bombas);
+  posibles_bombas = posibles_bombas + 1;
+endfunction
+
+function void decrementar_libres(inout integer casillas_libres);
+  casillas_libres = casillas_libres - 1;
+endfunction
+
+function void verificar_tablero_y_actualizar(input integer tablero[7:0][7:0], inout integer condicion_victoria);
+  integer i, j;
+  for (i = 0; i < 8; i = i + 1) begin
+    for (j = 0; j < 8; j = j + 1) begin
+      if (tablero[i][j] == -1) begin
+        $display("Encontrado -1, ¡ha perdido!");
+        condicion_victoria = -1; // Cambiar condicion_victoria a -1 (derrota)
+        return; // Puedes detener la búsqueda aquí si lo deseas
+      end
+    end
+  end
+  // Si no se encontró ningún -1, imprimir "1 no encontrado"
+  $display("1 no encontrado");
+endfunction
+
+
+function void verificar_tablero_victoria(input bit tablero_victoria[7:0][7:0], inout integer condicion_victoria);
+  integer i, j;
+  for (i = 0; i < 8; i = i + 1) begin
+    for (j = 0; j < 8; j = j + 1) begin
+      if (tablero_victoria[i][j] != 1) begin
+		$display("Encontrado 0, ¡siga jugando!");
+        return; // No todas las celdas son 1, no cambiamos condicion_victoria
+      end
+    end
+  end
+
+  // Si llegamos aquí, todas las celdas son 1
+  condicion_victoria = 1; // Establecemos condicion_victoria en 1
+  $display("buajaja ganó");
+endfunction
+  
+  
+
+
+function void realizar_movimiento(
+  inout integer tablero[7:0][7:0],
+  input bit m_bombas[7:0][7:0],
+  input integer m_perimetro[7:0][7:0],
+  input integer movimiento,
+  input integer posicion_x,
+  input integer posicion_y
+ 
+);
+  if (movimiento == 1) begin
+    if (m_bombas[posicion_x][posicion_y] == 1'b1) begin
+      tablero[posicion_x][posicion_y] = -1; // Colocar -1 en tablero si hay una bomba en esa posición
+    end else begin
+      if (m_bombas[posicion_x][posicion_y] == 1'b0) begin
+        if (m_perimetro[posicion_x][posicion_y] == 0) begin
+          tablero[posicion_x][posicion_y] = 9; // Poner 9 en tablero si m_perimetro es 0
+			 
+        end else begin
+          tablero[posicion_x][posicion_y] = m_perimetro[posicion_x][posicion_y]; // Copiar m_perimetro a tablero
+			 
+        end
+      end
+    end
+  end else if (movimiento == 2) begin
+    tablero[posicion_x][posicion_y] = -2; // Colocar -2 en tablero
+	 
+  end
+endfunction
 
   
 endmodule
--- Lab4/tb_matriz_bombas.sv
+++ Lab4/tb_matriz_bombas.sv
@@ -6,12 +6,20 @@ module tb_matriz_bombas;
   integer num_bombas = 10; // Inicializar num_bombas en 10
   integer i, j; // Variables de control
   integer casillas_libres; // Variable para almacenar el número de casillas libres
+  integer posibles_bombas = 5; // Inicializar posibles_bombas en 5
+  integer condicion_victoria = 0; // Inicializar posibles_bombas en 5
+  integer movimiento = 0;
+  integer posicion_x = 0;
+  integer posicion_y = 0;
+  
+  
 
   // Instanciar el módulo matriz_bombas
   matriz_bombas matriz_bombas_inst (
     .m_bombas(m_bombas), // Conectar la matriz m_bombas
     .m_perimetro(m_perimetro), // Conectar la matriz m_perimetro
-	 .casillas_libres(casillas_libres) // Conectar la salida de casillas_libres
+	 .casillas_libres(casillas_libres), // Conectar la salida de casillas_libres
+	 .posibles_bombas(posibles_bombas) // Conectar posibles_bombas
   );
 
   initial begin
@@ -26,6 +34,15 @@ module tb_matriz_bombas;
 	 
 	     // Llamamos a la función cantidad_libres para calcular el número de casillas libres
     matriz_bombas_inst.cantidad_libres(num_bombas, casillas_libres);
+	 
+	 	 
+	 // Mostrar el valor de posibles_bombas antes de llamar a la función
+    $display("Valor de posibles_bombas antes de llamar a la función: %d", posibles_bombas);
+	 
+	 // Llamar a la función incrementar_bombas
+    matriz_bombas_inst.incrementar_bombas(posibles_bombas);
+	 
+
 
     // Mostrar la matriz tablero
     $display("Matriz tablero:");
@@ -56,6 +73,117 @@ module tb_matriz_bombas;
 	 
 	     // Mostrar el número de casillas libres
     $display("Número de casillas libres: %d", casillas_libres);
+////////	 
+
+    // Realizar la primera prueba
+    movimiento = 1;
+    posicion_x = 0;
+    posicion_y = 0;
+
+    // Realizar operaciones en el tablero o configurar matrices necesarias
+    // ...
+
+    // Llamar a la función realizar_movimiento con las variables inicializadas
+    matriz_bombas_inst.realizar_movimiento(tablero, m_bombas, m_perimetro, movimiento, posicion_x, posicion_y);
+
+    // Mostrar la matriz tablero después de la primera prueba
+    $display("Resultado de la primera prueba:");
+    for (int i = 0; i < 8; i = i + 1) begin
+      for (int j = 0; j < 8; j = j + 1) begin
+        $write("%d ", tablero[i][j]);
+      end
+      $display(""); // Cambiar de línea para la siguiente fila
+    end
+
+    // Realizar la segunda prueba
+    movimiento = 1;
+    posicion_x = 1;
+    posicion_y = 2;
+
+    // Realizar operaciones en el tablero o configurar matrices necesarias
+    // ...
+
+    // Llamar a la función realizar_movimiento con las variables inicializadas
+    matriz_bombas_inst.realizar_movimiento(tablero, m_bombas, m_perimetro, movimiento, posicion_x, posicion_y);
+
+    // Mostrar la matriz tablero después de la segunda prueba
+    $display("Resultado de la segunda prueba:");
+    for (int i = 0; i < 8; i = i + 1) begin
+      for (int j = 0; j < 8; j = j + 1) begin
+        $write("%d ", tablero[i][j]);
+      end
+      $display(""); // Cambiar de línea para la siguiente fila
+    end
+
+    // Realizar la tercera prueba
+    movimiento = 1;
+    posicion_x = 7;
+    posicion_y = 7;
+
+    // Realizar operaciones en el tablero o configurar matrices necesarias
+    // ...
+
+    // Llamar a la función realizar_movimiento con las variables inicializadas
+    matriz_bombas_inst.realizar_movimiento(tablero, m_bombas, m_perimetro, movimiento, posicion_x, posicion_y);
+
+    // Mostrar la matriz tablero después de la tercera prueba
+    $display("Resultado de la tercera prueba:");
+    for (int i = 0; i < 8; i = i + 1) begin
+      for (int j = 0; j < 8; j = j + 1) begin
+        $write("%d ", tablero[i][j]);
+      end
+      $display(""); // Cambiar de línea para la siguiente fila
+    end
+
+    // Realizar la cuarta prueba
+    movimiento = 2;
+    posicion_x = 1;
+    posicion_y = 1;
+
+    // Realizar operaciones en el tablero o configurar matrices necesarias
+    // ...
+
+    // Llamar a la función realizar_movimiento con las variables inicializadas
+    matriz_bombas_inst.realizar_movimiento(tablero, m_bombas, m_perimetro, movimiento, posicion_x, posicion_y);
+
+    // Mostrar la matriz tablero después de la cuarta prueba
+    $display("Resultado de la cuarta prueba:");
+    for (int i = 0; i < 8; i = i + 1) begin
+      for (int j = 0; j < 8; j = j + 1) begin
+        $write("%d ", tablero[i][j]);
+      end
+      $display(""); // Cambiar de línea para la siguiente fila
+    end
+
+
+///////
+	     // Mostrar el valor de posibles_bombas después de llamar a la función
+    $display("Valor de posibles_bombas después de llamar a la función: %d", posibles_bombas);
+	 
+	 	 // Llamar a la función decrementar_libres pasando casillas_libres como argumento inout
+    matriz_bombas_inst.decrementar_libres(casillas_libres);
+
+	 
+	 	     // Mostrar el valor de posibles_bombas después de llamar a la función
+    $display("casillas libres luego de decrementar: %d", casillas_libres);
+	 
+	 // Llamar a la función para verificar el tablero y actualizar condicion_victoria
+    matriz_bombas_inst.verificar_tablero_y_actualizar(tablero, condicion_victoria);
+
+    // Modificar la matriz tablero para contener un valor igual a -1
+    tablero[3][4] = -1; // Por ejemplo, coloca un -1 en la posición (3, 4) del tablero
+
+    // Llamar a la función para verificar el tablero y actualizar condicion_victoria
+    matriz_bombas_inst.verificar_tablero_y_actualizar(tablero, condicion_victoria);
+	 
+	 
+	 
+	 
+	 
+
+	 
+	 
+	 
 
     // Finalizar la simulación
     $finish;
